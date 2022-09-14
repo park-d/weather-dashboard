af105c841a09195d87c93b986cb088eee39c6a91
park-d <54589582+park-d@users.noreply.github.com>
2022-09-13 21:34:13 -0500

    made a function to create cards to hold daily forecast data and then append to the 5 day forecast section

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,14 +51,15 @@ function forecastWeatherAPI(event) {
         let searchLocation = event.target.innerText;
         requestUrl = apiURL + `${forecastAPI}?q=${searchLocation}&appid=${apiKey}${unitsKey}`;
     }
-
+    //api fetch & promise
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            //function call here
+            addForecastCards(data);
+
         });
 }
 
@@ -78,6 +79,44 @@ function addCurrentData(data) {
     tempElement.text(`Temp: ${data.main.temp}${String.fromCharCode(176)}F`);
     windElement.text(`Wind: ${data.wind.speed} MPH`);
     humidityElement.text(`Humidity: ${data.main.humidity}%`);
+}
+
+// function to add the data from the API call to create new elements to display on screen
+function addForecastCards(data) {
+    forecastElements.empty();
+    // loop through the data array from API call to select one time from each day (because of the database we had to use) and display the required data on screen
+    for(let i = 3; i < (data.list.length); i = i + 8) {
+        let hiddenTitle = $('#forecast-title');
+        hiddenTitle.removeClass('hidden');
+
+        let forecastDate = data.list[i].dt_txt;
+        let forecastTemp = `Temp: ${data.list[i].main.temp}${String.fromCharCode(176)}`;
+        let forecastWind = `Wind: ${data.list[i].wind.speed} MPH`;
+        let forecastHumidity = `Humidity: ${data.list[i].main.humidity}%`;
+
+        let icon = data.list[i].weather[0].icon;
+        let iconImg = $('<img style; height=50px, width=50px>');
+        iconImg.attr('src', 'https://openweathermap.org/img/wn/' + icon + '@2x.png');
+        // generating a card to hold the elements and data for each day's forecast
+        let forecastCards = $('<div class = "card col-lg-2 col-md-4 col-sm-12 p-4  forecast-cards">');
+        // creating new elements to hold data from above
+        let cardDate = $('<h4>');
+        let cardTemp = $('<p>');
+        let cardWind = $('<p>');
+        let cardHumidity = $('<p>');
+        // adding the elements and data generated above to the daily cards
+        cardDate.text(moment(forecastDate).format('ll'));
+        forecastCards.append(cardDate);
+        forecastCards.append(iconImg);
+        cardTemp.text(forecastTemp);
+        forecastCards.append(cardTemp);
+        cardWind.text(forecastWind);
+        forecastCards.append(cardWind);
+        cardHumidity.text(forecastHumidity);
+        forecastCards.append(cardHumidity);
+        // adding the new cards to the div that houses the forecast
+        forecastElements.append(forecastCards);
+    }
 }
 
 fetchButton.click(currentWeatherAPI);
