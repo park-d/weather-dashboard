6175acf2de977680655e6d7a3fa33f28a6963dfa
park-d <54589582+park-d@users.noreply.github.com>
2022-09-13 20:54:42 -0500

    added all needed DOM elements, API variables, and an API call for the current weather API, and shows the data in the console on submit

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// DOM elements
+let fetchButton = $('#search-btn');
+let clearButton = $('#clear-btn');
+let searchElement = $('#search-input');
+let cityElement = $('#search-city');
+let tempElement = $('#temp');
+let windElement = $('#wind');
+let humidityElement = $('#humidity');
+let forecastElements = $('.forecast');
+
+// API variables
+let apiURL = 'https://api.openweathermap.org/data/2.5/';
+let currentAPI = 'weather';
+let forecastAPI = 'forecast';
+let apiKey = 'bc1d5e6f407cb15f1035b7596c15a052';
+let unitsKey = '&units=imperial';
+
+// function for current weather API & runs function to add data to screen, dynamic based on whether or not you click on history or new search
+function currentWeatherAPI(event) {
+    event.preventDefault();
+    let requestUrl;
+    //dynamic search query change
+    if(event.target.id === "search-btn") {
+        let searchLocation = searchElement.val();
+        if(searchLocation == '') {return;};
+        requestUrl = apiURL + `${currentAPI}?q=${searchLocation}&appid=${apiKey}${unitsKey}`;
+    } else {
+        let searchLocation = event.target.innerText;
+        requestUrl = apiURL + `${currentAPI}?q=${searchLocation}&appid=${apiKey}${unitsKey}`;
+    }
+    //api fetch & promise
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+           console.log(data);
+        });
+}
+
+fetchButton.click(currentWeatherAPI)
